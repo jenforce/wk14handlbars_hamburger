910f3153633c626f505e891ba05fe01d4dfcb953
jenward <jenforce@aol.com>
2016-08-04 18:09:23 -0400

    Thursday update setup hosted db

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require('../config/orm.js');
+
+var buger = {
+  all: function (cb) {
+    orm.all('bugers', function (res) {
+      cb(res);
+    });
+  },
+  // cols and vals are arrays
+  create: function (cols, vals, cb) {
+    orm.create('burgers', cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  update: function (objColVals, condition, cb) {
+    orm.update('burgers', objColVals, condition, function (res) {
+      cb(res);
+    });
+  }
+
+};
+
+module.exports = burger;
